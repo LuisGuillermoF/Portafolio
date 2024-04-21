20255d84aaa38ccf510732725d044b254a868868
Luis Guiller Foronda <morganixxD@gmail.com>
2024-04-21 10:45:46 -0500

    montando avances css y slider bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,6 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    colors:{
-      'primary': '#00b4d8',
-      'secondary': '#212529'
-    },
     extend: {},
   plugins: [],
 }
